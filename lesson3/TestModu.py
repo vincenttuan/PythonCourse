d7c5483e8fcf8d579eecfdc9aa524b6bde769358
vincentjava <vincentjava@yahoo.com.tw>
2019-03-16 10:06:30 +0800

    Python

---
+++ lesson3/TestModu.py
@@ -0,0 +1,4 @@
+import modu.Calc as c
+
+print(c.add(10, 20))
+print(c.lotto)
---
+++ modu/Calc.py
@@ -0,0 +1,16 @@
+lotto = (3, 10, 16, 23, 36, 41)
+
+num = 7
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def multi(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
