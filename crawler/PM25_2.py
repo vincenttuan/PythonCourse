68a463ac7311943ec3db4c72a05ccee8698d1939
vincentjava <vincentjava@yahoo.com.tw>
2019-03-23 10:26:29 +0800

    Python

--- crawler/PM25_2.py
+++ crawler/PM25_2.py
@@ -19,5 +19,15 @@ print(sp.find("a", {"href":"HourlyData.aspx"}).get('title').strip());
 rs = sp.find_all("tr", {"align":"center", "style":"border-width:1px;border-style:Solid;"})
 
 for r in rs:
-    print(r.text.strip())
+    #print(r.text.strip())
+    name = r.find('a').text.strip()
+    pm25 = r.find_all('span')
+
+    #print("%s %s %s" % (name, pm25[0].text.strip(), pm25[1].text.strip()))
+
+    print(name, end=" ")
+    for p in pm25:
+        print(p.text.strip(), end=" ")
+    print()
+
 
