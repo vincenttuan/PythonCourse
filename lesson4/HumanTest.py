db542b3ce445b2481642bdea081a379525d4d598
vincentjava <vincentjava@yahoo.com.tw>
2019-03-23 13:11:06 +0800

    Python

---
+++ lesson4/HumanTest.py
@@ -0,0 +1,16 @@
+class Human:
+    name = ''
+    age = 0
+    sex = ''
+    def __str__(self):
+        return self.name + ',' + str(self.age) + ',' + self.sex
+
+human = Human()
+human.name = 'Vincent'
+human.age = 20
+human.sex = '男'
+
+print(human)
+print(human.name)
+print(human.age)
+print(human.sex)
