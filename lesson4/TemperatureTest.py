f234ac41540c3bcc82474311d4d54399bfd13c69
vincentjava <vincentjava@yahoo.com.tw>
2019-03-23 16:05:51 +0800

    Python

--- lesson4/TemperatureTest.py
+++ lesson4/TemperatureTest.py
@@ -11,14 +11,14 @@ class Celsius: # 攝氏
     def __set__(self, instance, value):
         instance.fahrenheit = value * 9 / 5 + 32
 
+
 class Temperature: # 溫度
     celsius = Celsius()
     def __init__(self, f):
         self.fahrenheit = f # fahrenheit = 華氏
 
+
 t = Temperature(32)
 print(t.celsius) # __get__
 t.celsius = 100 # __set__
 print(t.fahrenheit)
-
-
