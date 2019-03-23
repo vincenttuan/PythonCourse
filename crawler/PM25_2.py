af8ecd585b7fff0247d49e563715463127bd557f
vincentjava <vincentjava@yahoo.com.tw>
2019-03-23 09:55:57 +0800

    Python

---
+++ crawler/PM25_2.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://taqm.epa.gov.tw/pm25/tw/PM25A.aspx?area=1'
+html = requests.get(url)
+
+#print(html.text)
+
+sp = BeautifulSoup(html.text, 'html.parser')
+print(len(sp.select("title")));
+print(sp.select("title")[0]);
+print(sp.select("title")[0].text);
+print(sp.select("title")[0].text.strip());
